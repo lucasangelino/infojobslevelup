a17498ccabae4ac3c647f890e851ec250a0f4460
Lucas Angelino <angelino.n.lucas@gmail.com>
2023-05-19 11:27:13 -0300

    add page layout

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -4,7 +4,7 @@ export const PageLayout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <main className="max-w-7xl mx-auto px-2">{children}</main>
+      <main className="max-w-7xl mx-auto px-2 py-5">{children}</main>
     </>
   );
 };
